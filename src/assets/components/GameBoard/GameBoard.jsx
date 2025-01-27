874a7891a18ea7565d7d5635e1c1d361a91c38d8
Tanmay <v.tanmay13@gmail.com>
2025-01-27 02:39:39 -0800

    Fixed Styling Issues-2

--- src/assets/components/GameBoard/GameBoard.jsx
+++ src/assets/components/GameBoard/GameBoard.jsx
@@ -80,11 +80,12 @@ const style = {
   top: "20%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "70%",
   bgcolor: "#46432f",
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  margin: "auto",
 };
 
 export default function GameBoard({ player1, player2, socket }) {
